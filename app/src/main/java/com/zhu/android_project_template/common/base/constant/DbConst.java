f9a089703356666dbc52d45726d6c59c18820357
Mr.Lee <2932582804@qq.com>
2017-03-16 14:59:56 +0800

    app----常量类新增

---
+++ app/src/main/java/com/zhu/android_project_template/common/base/constant/DbConst.java
@@ -0,0 +1,10 @@
+package com.zhu.android_project_template.common.base.constant;
+
+
+/**
+ * @ClassName: DbConst
+ * @description:
+ * @author:  Mr.Lee
+ */
+public final class DbConst {
+}
---
+++ app/src/main/java/com/zhu/android_project_template/common/base/constant/RemoteReqUrl.java
@@ -0,0 +1,22 @@
+package com.zhu.android_project_template.common.base.constant;
+
+
+/**
+ * @className: RemoteReqUrl
+ * @description: 后台接口请求地址
+ * @author  Mr.Lee
+ */
+@SuppressWarnings({"unused"})
+public final class RemoteReqUrl {
+
+    /**
+     * SERVER_IP：服务器IP地址
+     */
+    private static final String SERVER_IP = "";
+
+    /**
+     * TEST: 网络测试地址
+     */
+    public static final String TEST = "http://192.168.3.104:8082/";
+
+}
